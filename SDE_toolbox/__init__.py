a1a710c017b4d9d6c51166700e133fee9f8b5bcb
yyadu <112511838+yyadu@users.noreply.github.com>
2025-02-09 15:34:30 -0500

    Update __init__.py

--- SDE_toolbox/__init__.py
+++ SDE_toolbox/__init__.py
@@ -2,7 +2,6 @@ from .SPR import SPR
 from .PhaseBuilder2D import buildPsi2D
 from .utils import find_lambda_1, PsiFunc2D, MRTFunc2D, dxPsiFunc2D, dyPsiFunc2D, dxMRTFunc2D, dyMRTFunc2D, giveMeIsocrone, fourier_fit_array
 from .integrators import EulerODE, EulerSDE, longTermStats, HeunSDE
-from scipy.integrate import trapz
 from .coefficients import getRotationDiffusion
 from .coefficientsMRT import getRotationDiffusionMRT
 from .aiPRC import PRC_directMethod, aiPRC_empirical
